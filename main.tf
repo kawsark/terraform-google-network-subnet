7e6111a2c60009ac33d5eb60658079c5968cf7c0
kawsark <kawsar@hashicorp.com>
2020-04-13 09:47:43 -0400

    added gitignore and upgraded to 0.12

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "google_compute_subnetwork" "new_subnetwork" {
-  name          = "${var.name}"
-  region        = "${var.subnetwork-region}"
-  network       = "${var.vpc}"
-  ip_cidr_range = "${var.ip_cidr_range}"
-  description   = "${var.description}"
+  name          = var.name
+  region        = var.subnetwork-region
+  network       = var.vpc
+  ip_cidr_range = var.ip_cidr_range
+  description   = var.description
 }
+
